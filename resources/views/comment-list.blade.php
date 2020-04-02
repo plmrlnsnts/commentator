75dd9b587f2d9cf61a6aaa985462e3a2073121bb
Paul Santos <paulmarlonsantos@gmail.com>
2020-04-02 22:13:52 +0800

    wip

--- resources/views/comment-list.blade.php
+++
@@ -1,9 +0,0 @@
-<div>
-    @forelse($comments as $comment)
-        @livewire('commentator::comment-list-item', compact('comment'), key($comment->id))
-    @empty
-        <div class="text-center text-gray-600 py-8">
-            Be the first to comment.
-        </div>
-    @endforelse
-</div>
